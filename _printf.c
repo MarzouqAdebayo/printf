ba177ef8a5f54f51d1b52a683b030bfe137bf37d
tim ermyas <45384341+tim6554@users.noreply.github.com>
2023-08-21 15:58:34 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,82 +1,66 @@
 #include "main.h"
 
-void dummy(void);
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - mimics the printf function from stdio library
- * @format:  String with/without specifiers. The format string is composed of
- *  zero or more directives
- *
- * Return: int - length of the string
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
-
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, len = 0;
-	char *word;
-	va_list args;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-	if (!format)
+	if (format == NULL)
 		return (-1);
 
-	va_start(args, format);
-	while (format[i])
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] != '%')
 		{
-			if (('a' <= format[i + 1] && format[i + 1] <= 'z') || format[i + 1] == '%')
-			{
-				switch (format[i + 1])
-				{
-				case 'c':
-					_putchar(va_arg(args, int));
-					len += 1;
-					i += 2;
-					break;
-				case 's':
-					word = va_arg(args, char *);
-					if (!word)
-						return (-1);
-					len += print_string(word);
-					i += 2;
-					break;
-				case '%':
-					_putchar('%');
-					i += 2;
-					len += 1;
-					break;
-				case 'd':
-					len += print_decimal(va_arg(args, int));
-					i += 2;
-					break;
-				case 'i':
-					len += print_decimal(va_arg(args, int));
-					i += 2;
-					break;
-				case 'b':
-					len += print_binary(va_arg(args, int));
-					i += 2;
-					break;
-				}
-			}
-			else
-				return (-1);
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
 		}
 		else
 		{
-			_putchar(format[i]);
-			i++;
-			len++;
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
 		}
 	}
-	va_end(args);
-	return (len);
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
 
 /**
- * dummy - to trick betty
- * Return: void
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
  */
-void dummy(void)
+void print_buffer(char buffer[], int *buff_ind)
 {
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
 }
